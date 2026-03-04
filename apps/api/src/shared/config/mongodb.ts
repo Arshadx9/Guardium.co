cd50f8e50fd0b68a237bc64509ab917cc3f00100
name <arshadalib007@gmail.com>
2026-03-04 16:11:29 +0530

    added mongodb singleton connection manager

---
+++ apps/api/src/shared/config/mongodb.ts
@@ -0,0 +1,54 @@
+import config from "./index.js"
+import mongoose from "mongoose"
+import logger from "./logger.js"
+
+class MongoConnection {
+
+    connection : mongoose.Connection | null
+
+    constructor (){
+        this.connection = null ;
+    }
+
+
+async connect () {
+
+if(this.connection){
+    logger.info("connection already exists")
+    return this.connection
+}
+
+await mongoose.connect(config.mongo.uri)
+
+this.connection = mongoose.connection
+
+logger.info(`connected to Mongodb ${config.mongo.uri}`)
+
+this.connection.on("error" ,(err) =>{
+ logger.error("MongoDB connection error", err)
+})
+
+ this.connection.on("disconnected", () => {
+                logger.error("MongoDB Disconnected")
+            })
+
+            return this.connection
+}
+
+async diconnect () {
+    if(this.connection){
+       await mongoose.disconnect()
+        this.connection = null
+        logger.info("mongodb disconnected")
+    }
+
+}
+
+async getconnection() {
+    return this.connection
+}
+
+}
+
+export default new MongoConnection()
+
