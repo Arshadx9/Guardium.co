a12dcc8e9d9b579edc4c307d9ebbcd7320cf3544
name <arshadalib007@gmail.com>
2026-03-06 16:17:01 +0530

    added apikey database file

---
+++ apps/api/src/shared/models/apikeys.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const apikeySchema = new Schema({
+    key : {type : String , required : true},
+    clientID :{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'bizowner'
+    },
+    isActive : {type : Boolean , default: true},
+
+    expiresAt : {
+    type : Date , 
+
+    default : () =>{
+        return new Date( Date.now() +  365 * 24 * 60 * 60 * 1000)
+    }
+
+    },
+
+    name : {type : String , required : true} , 
+
+    createdBy : {type : mongoose.Schema.Types.ObjectId , ref : 'bizowner' , required : true },
+
+    
+
+
+} , {timestamps : true})
