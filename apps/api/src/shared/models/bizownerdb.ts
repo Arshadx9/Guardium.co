71ddd80a19043ae8b6abbb40ef820ee144cb687c
name <arshadalib007@gmail.com>
2026-03-06 13:06:12 +0530

    added the client database file

---
+++ apps/api/src/shared/models/bizownerdb.ts
@@ -0,0 +1,15 @@
+import mongoose, { model, Schema } from "mongoose";
+
+
+
+
+const bizownerSchema = new Schema({
+    username : {type : String , unique : true},
+    password : {type : String },
+    isActive : {type : Boolean , default : true} , 
+
+})
+
+const bizowner = model("bizowner" , bizownerSchema)
+
+export default bizowner
