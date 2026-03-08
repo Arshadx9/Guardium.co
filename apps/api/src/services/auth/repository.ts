8f9e8eb449065ada64659b3a1d32476007344ada
name <arshadalib007@gmail.com>
2026-03-08 21:47:56 +0530

    added the auth repositry file that interacts with database

---
+++ apps/api/src/services/auth/repository.ts
@@ -0,0 +1,37 @@
+import apikey from "../../shared/models/apikeys.js"
+import bizowner from "../../shared/models/bizownerdb.js"
+
+
+export const findByUsername = async (enteredusername : string) => {
+     
+ return await bizowner.findOne({
+    username : enteredusername 
+  }).select('-password')
+
+ 
+
+}
+
+export const findusernamewithpassword = async(enteredusername : string ) => {
+    return await bizowner.findOne({
+        username : enteredusername,
+    })
+
+}
+
+export const createUser = async (enteredusername : string  , enteredpassword : string ) => {
+
+
+   return   await bizowner.create({
+        username : enteredusername,
+        password : enteredpassword
+    })
+
+}
+
+export const createapikey = async (enteredapikey : string, enteredClientID : string) => {
+   return await apikey.create({
+        key : enteredapikey,
+        clientID : enteredClientID
+    })
+}
