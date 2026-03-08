349db528796541fb40f81e4882c4bbd5b66d7383
name <arshadalib007@gmail.com>
2026-03-09 00:03:47 +0530

    added the logic for register and login button

---
+++ apps/api/src/services/auth/authservice.ts
@@ -0,0 +1,59 @@
+import apikey from "../../shared/models/apikeys.js";
+import AppError from "../../shared/Utils/Apperror.js"
+import Securityutils from "../../shared/Utils/Securityutils.js"
+import { createUser, findByUsername , createapikey , findusernamewithpassword } from "./authrepository.js"
+import crypto from "crypto";
+
+
+
+export const register = async (enteredusername : string ,enteredpassword : string ) => {
+
+
+ const existingUser = await findByUsername(enteredusername)
+    if (!existingUser) {
+        throw new AppError("Username already taken", 400)
+    }
+
+const hashedpass = await Securityutils.hashpassword(enteredpassword)
+
+
+ const newuser = await createUser(enteredusername , hashedpass)
+
+const generatedapiKey = `grd_${crypto.randomBytes(32).toString('hex')}`
+
+await createapikey(generatedapiKey  , newuser._id.toString() )
+
+ const generatedtoken =  Securityutils.generateToken({userId : newuser._id}) 
+
+return({
+    user : newuser,
+    apiKey : generatedapiKey ,
+    token: generatedtoken
+})
+
+}
+
+export const login = async (enteredusername : string , enteredpassword : string) => {
+
+ const existingUser =  await findusernamewithpassword(enteredusername )
+  
+ if (!existingUser || !existingUser.password) {
+        throw new AppError("Invalid credentials", 401)
+    }
+
+
+
+const isMatch = await Securityutils.comparepassword(enteredpassword , existingUser.password)
+
+if (!isMatch) {
+    throw new AppError("Invalid credentials", 401)
+}
+
+const generatedtoken =  Securityutils.generateToken({userId : existingUser._id})
+
+return({
+    user : existingUser,
+    token: generatedtoken
+})
+
+}
