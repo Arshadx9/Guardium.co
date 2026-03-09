5aed759f1ec1622c561ea9ce65246ad4cb3d59d6
name <arshadalib007@gmail.com>
2026-03-09 22:11:54 +0530

    added the ingest controller file

---
+++ apps/api/src/services/Ingest/Ingestcontroller.ts
@@ -0,0 +1,15 @@
+import type { NextFunction, Request, Response } from "express"
+import { processHit } from "./ingestservice.js"
+import ResponseFormatter from "../../shared/Utils/Responseformatter.js"
+export const ingestcontroller = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { apiKey, endpoint, method, statusCode, latencyMs, ip, timestamp } = req.body
+
+        await processHit(apiKey, endpoint, method, statusCode, latencyMs, ip, timestamp)
+
+        res.status(200).json(ResponseFormatter.success(null, "Hit recorded"))
+
+    } catch (error) {
+        next(error)
+    }
+}
