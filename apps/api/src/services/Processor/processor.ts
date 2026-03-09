cac56db6aefe6252aa107410b4de97e7b5214a70
name <arshadalib007@gmail.com>
2026-03-10 00:16:20 +0530

    added the processor file that does the background job

---
+++ apps/api/src/services/Processor/processor.ts
@@ -0,0 +1,37 @@
+import rabbitmqConnection from "../../shared/config/rabbitmq.js"
+import AppError from "../../shared/Utils/Apperror.js"
+import config from "../../shared/config/index.js"
+import apihit from "../../shared/models/apihits.js"
+import logger from "../../shared/config/logger.js"
+
+export const startprocess = async () => {
+
+
+
+ const channel = rabbitmqConnection.getChannel()
+
+  if (!channel) {
+          throw new AppError("RabbitMQ not connected", 500)
+    }
+
+channel.consume(config.rabbitmq.queue , async (msg)=>{
+
+        if (!msg) return
+
+        try{
+   const hitData = JSON.parse(msg.content.toString())
+    await apihit.create(hitData)
+    channel.ack(msg)
+            logger.info("Hit saved to MongoDB")
+        }
+    catch(error){
+ logger.error("Failed to process hit", error)
+            channel.nack(msg, false, false)
+    }
+})
+
+    logger.info("Processor started, listening to queue:", config.rabbitmq.queue)
+
+
+}
+
