46a25b234b9cc95de51575df4059a80ca158de08
name <arshadalib007@gmail.com>
2026-03-10 16:04:29 +0530

    added aggregation pipeline for latency and error rate

--- apps/api/src/services/Analytics/analytics.ts
+++ apps/api/src/services/Analytics/analytics.ts
@@ -13,3 +13,28 @@ export const Hitsbyendpoint = async () => {
         ]
     )
 }
+
+export const avglatperendpoint = async () => {
+    return await apihit.aggregate([
+        { $group : { _id : "$endpoint" , count : {$sum : 1}, avgLatency : {$avg : "$latencyMs"}        }},
+        {$sort : {avgLatency : -1}}
+        
+    ])
+}
+
+export const errorrateperendpoint = async () => {
+    return await apihit.aggregate([
+        {$group : {_id : "$endpoint" , errorcount :{ $sum :{
+              $cond: [{ $gte: ["$statusCode", 400] }, 1, 0] 
+        } } }},
+
+        { $project: { 
+    errorRate: { 
+        $multiply: [
+            { $divide: ["$errorCount", "$totalHits"] }, 
+            100
+        ] 
+    }
+}}
+    ])
+}
