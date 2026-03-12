7219cf083ba6f6e2a7705d068fee3b1fc1b33665
name <arshadalib007@gmail.com>
2026-03-13 00:42:22 +0530

    added navbar component

---
+++ apps/web/Components/Navbar.tsx
@@ -0,0 +1,37 @@
+
+
+
+const Navbar = () =>{
+
+    return(
+          <div className="bg-white  px-8 py-4 shadow-mauve-300 flex justify-between items-center rounded-3xl">
+
+
+<div className="text-3xl text-black tracking-tight ">
+Guardium
+</div>
+
+<div className="text-black flex gap-7 ">
+
+    <a href="#">Features</a>
+    <a href="#">How it works</a>
+
+</div>
+
+
+
+<div>
+<button className="px-4 py-2 rounded-lg bg-black text-white font-mono tracking-tighter">
+Start for free
+</button>
+</div>
+
+
+    </div>
+    )
+
+  
+
+}
+
+export default Navbar
