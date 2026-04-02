8ddbb1d5a852f72617c8eacaf1da1f8a28fd9e27
name <arshadalib007@gmail.com>
2026-04-02 15:56:51 +0530

    added register and login pages.

---
+++ apps/web/app/register/page.tsx
@@ -0,0 +1,59 @@
+"use client"
+import { useRef } from "react"
+import axios, { AxiosError } from "axios";
+import { useRouter } from "next/navigation"
+
+
+const Register = () => {
+      const router = useRouter()
+
+
+const usernameRef = useRef <HTMLInputElement | null >(null)
+const passwordRef = useRef <HTMLInputElement | null > (null)
+
+
+async function register(){
+  
+    const username = usernameRef.current?.value
+    const password = passwordRef.current?.value
+
+    try{
+      const response =   await axios.post("http://localhost:5000/api/auth/register", {
+        username , 
+        password
+      }, {
+        withCredentials: true
+      })
+     console.log(response.data)
+     
+
+        router.push("/login")
+
+
+
+    } catch (err: unknown) {
+  const error = err as AxiosError<{ message: string }>;
+
+  alert(error.response?.data?.message || "Signup failed");
+}
+
+}
+
+return(
+    <div className="h-screen w-screen flex flex-col justify-center items-center gap-4 bg-[rgb(240,235,229)]">
+  
+  <input ref={usernameRef} placeholder="username" className="border text-black p-2 font-mono  "    />
+  <input ref={passwordRef} placeholder="password" className="border text-black p-2 font-mono  "    />
+
+
+<button onClick={register} className="px-4 py-2 rounded-lg bg-gray-100 border text-black font-mono tracking-tighter" >
+Register
+</button>
+
+    </div>
+)
+
+
+}
+
+export default Register
